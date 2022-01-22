ce9bbca797479692b1de0177ea1291b70c395e20
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-22 21:43:24 +0100

    threads

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let mut c = vec![];
+
+    for i in 0..10 {
+        c.push(thread::spawn(move || {
+            println!("thread number {}", i);
+        }))
+    }
+
+    for j in 0..10 {
+        println!("main thread number {}", j);
+    }
 }
